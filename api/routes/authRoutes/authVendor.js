87351bbf16c7b866471b9795f7a3d8b82685840d
aditya37 <aditrahman212@gmail.com>
2019-11-21 03:30:15 +0700

    add documentation with apidoc

--- api/routes/authRoutes/authVendor.js
+++ api/routes/authRoutes/authVendor.js
@@ -4,6 +4,51 @@ const bycrpt  = require('bcrypt');
 
 const Vendor = require('../../model/vendor/modelvendor');
 
+/**
+ * @api {post} auth/authVendor
+ * @apiName AuthVendor
+ * @apiGroup Vendor
+ * @apiDescription 
+ * Auth berfungsi untuk proses autentikasi akun vendor, baik vendor lama atau vendor yang baru terdaftar dalam sistem aplikasi ini
+ * jika proses autentikasi berhasil nanti akan muncul pesan atau respon berupa json, respon tersebut berfungsi untuk proses selanjutnya
+ * 
+ * @apiParam {String} Username username vendor yang terdaftar
+ * @apiParam {String} Password
+ * 
+ * @apiParamExample {json} Request-Example(json):
+ *    {
+ *      "username":"vendorTes",
+ *      "password":"password"
+ *    }
+ * 
+ * 
+ * @apiSuccess {String} _id Id Vendor
+ * @apiSuccess {String} username Username Vendor
+ * @apiSuccess {String} email Emaail Vendor
+ * @apiSuccess {String} password Password Vendor Yang Sudah dienkripsi
+ * @apiSuccess {Date} dateCreated Tanggal Registrasi Vendor
+ * @apiSuccess {Date} dateUpdated Tanggal vendor melakukan perubahan akun </br> ex: Update Username atau ganti kata sandi
+ * @apiSuccessExample {json} Success Auth
+ * {
+ *     "success":"1"
+ *     "message": "Login Sucessfully",
+ *     "vendor": [
+ *        {
+ *             "_id": "5dd59c87cf1f4129a5b9d060",
+ *             "username": "vendorTes",
+ *             "email": "vendorTes@gmail.com",
+ *             "password": "$2b$10$SlhI.v.hhqwJaOWKp.ZMC.GMt2NP2HC17JLhqD8kLteX49Vq3pkKm",
+ *             "dateCreated": "2019-02-22T00:00:00.000Z",
+ *             "dateUpdated": "2019-03-22T00:00:00.000Z"
+ *        }
+ *     ]
+ * }
+ * @apiSuccessExample {json} Failed Auth
+ * {
+ *  "Success": "0"
+ *  "message": "Gagal"
+ * }
+ */
 // Get All Customer datas
 router.post('/',(req,res,next)=>{
   Vendor.find({username:req.body.username}).exec()
---
+++ docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Waroenk API documentation",
+  "url": "http://localhost:3000/",
+  "name": "restapi-waroenk",
+  "version": "1.0.0",
+  "description": "restapi app-waroenk",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-11-20T20:29:25.623Z",
+    "url": "http://apidocjs.com",
+    "version": "0.18.0"
+  }
+});
